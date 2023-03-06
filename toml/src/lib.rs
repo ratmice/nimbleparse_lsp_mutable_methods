14f488169b2b4ea0bdfe6d152422cbc546c496f3
matt rice <ratmice@gmail.com>
2023-03-05 17:45:19 -0800

    Prototype of nimbleparse on the tower_lsp support-mutable-methods branch
    This currently just holds the Cargo.toml and generic libraries
    not impacted by the changes to the `LanguageServer` trait.
    
    The actual `LanguageServer` impl server/src/main.rs is just populated
    with the most basic LanguageServer example from the Readme.

---
+++ toml/src/lib.rs
@@ -0,0 +1,56 @@
+use lrpar::parser::RecoveryKind;
+use serde::{Deserialize, Serialize};
+use std::path::PathBuf;
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct Workspace {
+    pub parsers: toml::Spanned<Vec<Parser>>,
+    pub tests: Vec<TestDir>,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct Parser {
+    pub l_file: toml::Spanned<PathBuf>,
+    pub y_file: toml::Spanned<PathBuf>,
+    pub extension: toml::Spanned<String>,
+    // Spanned doesn't have a public constructor
+    // So it doesn't play nice with
+    #[serde(default = "default_yacc_kind")]
+    pub yacc_kind: cfgrammar::yacc::YaccKind,
+    #[serde(default = "default_recovery_kind")]
+    pub recovery_kind: RecoveryKind,
+    #[serde(default)]
+    pub quiet: bool,
+}
+
+fn default_yacc_kind() -> cfgrammar::yacc::YaccKind {
+    cfgrammar::yacc::YaccKind::Original(cfgrammar::yacc::YaccOriginalActionKind::GenericParseTree)
+}
+
+fn default_recovery_kind() -> RecoveryKind {
+    RecoveryKind::CPCTPlus
+}
+
+// A couple of more TestKinds we could consider in the future
+// are source generators, and files which contain a list of strings each to be parsed as
+// it's own file... So the reason TestKind is like this is for future expansion.
+// We should consider having a trait for it...
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub enum TestKind {
+    Dir(String),
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct TestDir {
+    pub kind: toml::Spanned<TestKind>,
+    pub pass: bool,
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+}
---
+++ xtask/src/main.rs
@@ -0,0 +1,73 @@
+mod flags;
+
+use std::env;
+use std::path::{Path, PathBuf};
+use xshell::{cmd, Shell};
+fn project_root() -> PathBuf {
+    Path::new(
+        &env::var("CARGO_MANIFEST_DIR").unwrap_or_else(|_| env!("CARGO_MANIFEST_DIR").to_owned()),
+    )
+    .ancestors()
+    .nth(1)
+    .unwrap()
+    .to_path_buf()
+}
+
+impl flags::Install {
+    pub(crate) fn run(self, sh: &Shell) -> anyhow::Result<()> {
+        let default = !(self.server || self.client);
+        let debug_args = if self.debug { Some("--debug") } else { None };
+
+        if self.server || default {
+            let server_args = vec!["--path", "server"];
+            if self.console {
+                let console_args = vec!["--features", "console"];
+                let rustc_flags = std::env::var("RUSTCFLAGS");
+                let _pushenv_guard = if let Ok(mut rustc_flags) = rustc_flags {
+                    rustc_flags.push_str("--cfg tokio_unstable");
+                    sh.push_env("RUSTFLAGS", rustc_flags)
+                } else {
+                    sh.push_env("RUSTFLAGS", "--cfg tokio_unstable")
+                };
+                cmd!(
+                    sh,
+                    "cargo install {debug_args...} {server_args...} {console_args...}"
+                )
+                .run()?;
+            } else {
+                cmd!(sh, "cargo install {debug_args...} {server_args...}").run()?;
+            }
+        }
+
+        if self.client || default {
+            let _dir = sh.push_dir("./vscode");
+            cmd!(sh, "npm ci").run()?;
+            cmd!(sh, "npm run package --scripts-prepend-node-path").run()?;
+            cmd!(sh, "code --install-extension nimbleparse_lsp.vsix --force").run()?;
+        }
+
+        Ok(())
+    }
+}
+
+impl flags::Clean {
+    pub(crate) fn run(self, sh: &Shell) -> anyhow::Result<()> {
+        {
+            let _dir = sh.push_dir("./vscode");
+            cmd!(sh, "npm run clean").run()?;
+        }
+        cmd!(sh, "cargo clean").run()?;
+        Ok(())
+    }
+}
+
+fn main() -> anyhow::Result<()> {
+    let sh = &Shell::new()?;
+    sh.change_dir(project_root());
+    let flags = flags::Xtask::from_env()?;
+
+    match flags.subcommand {
+        flags::XtaskCmd::Install(cmd) => cmd.run(sh),
+        flags::XtaskCmd::Clean(cmd) => cmd.run(sh),
+    }
+}
